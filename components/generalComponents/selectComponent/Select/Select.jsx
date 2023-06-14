d3149867f2bd76c668946c2d0aa8414578a75172
Blessed0314 <christian.daniel999@gmail.com>
2023-06-14 15:47:54 -0500

    fix dashboard se añade useRef a los selectores

--- components/generalComponents/selectComponent/Select/Select.jsx
+++ components/generalComponents/selectComponent/Select/Select.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./Select.module.css";
 
-function Select({ options, name }) {
+function Select({ options, name, onChange }) {
+  const handleSelectChange = (event) => {
+    const selectedValue = event.target.value;
+    onChange(name, selectedValue);
+  };
   return (
     <div>
-      <select name={name} id={name} className={styles.select}>
+      <select name={name} id={name} className={styles.select} onChange={handleSelectChange}>
         {options.map((option) => (
           <option value={option.id} key={option.id} className={styles.value}>
             {option.name}
--- components/generalComponents/selectComponent/SelectContainer/SelectContainer.jsx
+++ components/generalComponents/selectComponent/SelectContainer/SelectContainer.jsx
@@ -1,12 +1,19 @@
 import filtersDB from "../../../../helpers/provisionalFiltersDB";
 import Select from "../Select/Select";
 import styles from "./SelectContainer.module.css";
+import React, { useRef } from "react";
 
-const SelectsContainer = () => {
+const SelectsContainer = ({ onSelectChange }) => {
+  const selectedValuesRef = useRef({});
+
+  const handleSelectChange = (name, value) => {
+    selectedValuesRef.current[name] = value;
+    onSelectChange(selectedValuesRef.current);
+  };
   return (
     <div className={styles.Container}>
       {Object.keys(filtersDB).map((key) => (
-        <Select key={key} name={key} options={filtersDB[key]} />
+        <Select key={key} name={key} options={filtersDB[key]} onChange={handleSelectChange}/>
       ))}
     </div>
   );

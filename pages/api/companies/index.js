b22b7a84e52ee8fa3ea3aa30d371f771ed78332e
Jorge2 <jorgeacostadeleon11@yahoo.com>
2023-06-15 15:05:51 -0300

    add companies endpoint

--- pages/api/companies/index.js
+++ pages/api/companies/index.js
@@ -28,17 +28,36 @@ export default async function handler(req, res) {
       return res.status(201).json(newCompany);
     } catch (error) {
       console.error("Error creating company:", error);
-      return res.status(500).json({ error: "Error creating company" });
+      return res.status(500).json({ error: error.message });
     }
-  }
+  };
 
   if (req.method === "GET") {
-    try {
-      const allCompanies = await prisma.company.findMany();
-      return res.status(200).json(allCompanies);
-    } catch (error) {
-      console.error("Error retrieving companies:", error);
-      return res.status(500).json({ error: "Error retrieving companies" });
+    const { country } = req.query;
+    if (country) {
+      try {
+        const companies = await prisma.company.findMany({
+          where: {
+            country: {
+              equals: country
+            }
+          }
+        });
+  
+        return res.status(200).json(companies);
+      } catch (error) {
+        console.error("Error retrieving companies:", error);
+        return res.status(500).json({ error: error.message });
+      }
+    } else {
+      try {
+        const allCompanies = await prisma.company.findMany();
+        return res.status(200).json(allCompanies);
+      } catch (error) {
+        console.error("Error retrieving companies:", error);
+        return res.status(500).json({ error: error.message });
+      }
     }
   }
+  return res.status(405).json({ error: "Method Not Allowed" });
 };

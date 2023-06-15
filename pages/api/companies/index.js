c662788357c69417ab6233e15e4308e3f8eb860e
Jorge2 <jorgeacostadeleon11@yahoo.com>
2023-06-15 13:54:16 -0300

    add try catch to companies endpoint

--- pages/api/companies/index.js
+++ pages/api/companies/index.js
@@ -2,18 +2,8 @@ import prisma from "@/prisma/client";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const {
-      user,
-      name,
-      lastName,
-      email,
-      password,
-      country,
-      logo_Company
-    } = req.body;
-
-    const newUser = await prisma.company.create({
-      data: {
+    try {
+      const {
         user,
         name,
         lastName,
@@ -21,15 +11,34 @@ export default async function handler(req, res) {
         password,
         country,
         logo_Company
-      },
-    });
+      } = req.body;
 
-    return res.status(201).json(newUser);
+      const newUser = await prisma.company.create({
+        data: {
+          user,
+          name,
+          lastName,
+          email,
+          password,
+          country,
+          logo_Company
+        },
+      });
+
+      return res.status(201).json(newUser);
+    } catch (error) {
+      console.error("Error creating user:", error);
+      return res.status(500).json({ error: "Error creating user" });
+    }
   }
 
   if (req.method === "GET") {
-    const allCompanies = await prisma.company.findMany();
-  
-    return res.status(201).json(allCompanies)
+    try {
+      const allCompanies = await prisma.company.findMany();
+      return res.status(200).json(allCompanies);
+    } catch (error) {
+      console.error("Error retrieving companies:", error);
+      return res.status(500).json({ error: "Error retrieving companies" });
+    }
   }
 };
